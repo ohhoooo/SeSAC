137f741a357adcde0602342c26e7f30720282728
KJH <gh7052@gmail.com>
2024-12-26 21:25:24 +0900

    add: SeSAC-EmotionDiary

---
+++ SeSAC-EmotionDiary/SeSAC-EmotionDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC-EmotionDiary
+//
+//  Created by 김정호 on 12/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
