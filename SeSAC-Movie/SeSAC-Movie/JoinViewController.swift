73db8ccd0be8019fae1092ee69a1c69059e01652
KJH <gh7052@gmail.com>
2024-12-26 21:06:21 +0900

    feat(SeSAC-Movie): Code로 UI 속성 구현

---
+++ SeSAC-Movie/SeSAC-Movie/JoinViewController.swift
@@ -0,0 +1,94 @@
+//
+//  JoinViewController.swift
+//  SeSAC-Movie
+//
+//  Created by 김정호 on 12/26/24.
+//
+
+import UIKit
+
+final class JoinViewController: UIViewController {
+    
+    // MARK: - properties
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var nicknameTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var recommendationTextField: UITextField!
+    @IBOutlet weak var joinButton: UIButton!
+    @IBOutlet weak var additionalInformationLabel: UILabel!
+    @IBOutlet weak var additionalInformationSwitch: UISwitch!
+    
+    private lazy var textFieldArray = [idTextField,
+                                passwordTextField,
+                                nicknameTextField,
+                                locationTextField,
+                                recommendationTextField]
+    
+    private let placeholderMap = [0: "이메일 주소 또는 전화번호",
+                            1: "비밀번호",
+                            2: "닉네임",
+                            3: "위치",
+                            4: "추천 코드 입력"]
+    
+    // MARK: - life cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    // MARK: - methods
+    func configureUI() {
+        // View
+        view.backgroundColor = .black
+        
+        // Label
+        titleLabel.text = "JEONGFLIX"
+        titleLabel.textColor = UIColor(red: 236/255, green: 83/255, blue: 66/255, alpha: 1)
+        titleLabel.font = .systemFont(ofSize: 28, weight: .black)
+        
+        additionalInformationLabel.text = "추가 정보 입력"
+        additionalInformationLabel.textColor = .white
+        additionalInformationLabel.font = .systemFont(ofSize: 15, weight: .semibold)
+        
+        // TextField
+        textFieldArray.enumerated().forEach { (i, e) in
+            e?.tintColor = .white
+            e?.textColor = .white
+            e?.textAlignment = .center
+            e?.backgroundColor = UIColor(red: 85/255, green: 85/255, blue: 85/255, alpha: 1)
+            e?.setPlaceholder(placeholder: placeholderMap.filter { $0.key == i }.values.first!, color: .white)
+        }
+        
+        passwordTextField.isSecureTextEntry = true
+        
+        // Button
+        let attributes: [NSAttributedString.Key: Any] = [.font: UIFont.systemFont(ofSize: 16, weight: .black),
+                                                    .foregroundColor: UIColor.black]
+        let attributedTitle = NSAttributedString(string: "회원가입", attributes: attributes)
+        joinButton.setAttributedTitle(attributedTitle, for: .normal)
+        joinButton.backgroundColor = .white
+        joinButton.layer.cornerRadius = 6
+        joinButton.clipsToBounds = true
+        joinButton.configuration?.contentInsets = .init(top: 14, leading: 0, bottom: 14, trailing: 0)
+        
+        // Switch
+        additionalInformationSwitch.onTintColor = UIColor(red: 207/255, green: 79/255, blue: 64/255, alpha: 1)
+        additionalInformationSwitch.backgroundColor = .lightGray
+        additionalInformationSwitch.layer.cornerRadius = 16
+    }
+    
+    @IBAction func tappedJoinButton(_ sender: UIButton) {
+        view.endEditing(true)
+    }
+}
+
+// MARK: - extension
+extension UITextField {
+    func setPlaceholder(placeholder: String, color: UIColor) {
+        attributedPlaceholder = NSAttributedString(string: placeholder,
+                                            attributes: [.foregroundColor: color])
+    }
+}
