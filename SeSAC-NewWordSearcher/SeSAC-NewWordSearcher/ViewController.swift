a01c7980a0c7c232a14d0fcc61cdec88e4577b5d
KJH <gh7052@gmail.com>
2024-12-30 03:07:13 +0900

    feat(SeSAC-NewWordSearcher): Storyboard로 UI 구성 & Code로 UI 속성 구현

--- SeSAC-NewWordSearcher/SeSAC-NewWordSearcher/ViewController.swift
+++ SeSAC-NewWordSearcher/SeSAC-NewWordSearcher/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    // MARK: - properties
+    @IBOutlet weak var textFieldBackgroundView: UIView!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var resultBackgroundImageView: UIImageView!
+    
+    private let newWordArray = [""]
+    
+    // MARK: - life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureUI()
     }
-
-
+    
+    // MARK: - methods
+    private func configureUI() {
+        configureView()
+        configureTextField()
+        configureButton()
+        configureLabel()
+        configureImageView()
+    }
+    
+    private func configureView() {
+        textFieldBackgroundView.layer.borderWidth = 2
+    }
+    
+    private func configureTextField() {
+        searchTextField.borderStyle = .none
+        searchTextField.placeholder = "검색어를 입력하세요."
+    }
+    
+    private func configureButton() {
+        searchButton.setTitle("", for: .normal)
+        searchButton.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        searchButton.tintColor = .white
+        searchButton.backgroundColor = .black
+    }
+    
+    private func configureLabel() {
+        resultLabel.text = ""
+    }
+    
+    private func configureImageView() {
+        resultBackgroundImageView.image = .background
+    }
 }
-
