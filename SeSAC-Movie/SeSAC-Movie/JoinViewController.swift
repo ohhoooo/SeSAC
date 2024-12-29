7df598e59aaf5df3ffe62e470cfe7106daeb1fd8
KJH <gh7052@gmail.com>
2024-12-30 00:24:39 +0900

    refactor(SeSAC-Movie): Outlet Collection 사용, 메소드 세부 분리, extension 추가

--- SeSAC-Movie/SeSAC-Movie/JoinViewController.swift
+++ SeSAC-Movie/SeSAC-Movie/JoinViewController.swift
@@ -11,20 +11,10 @@ final class JoinViewController: UIViewController {
     
     // MARK: - properties
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var idTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var nicknameTextField: UITextField!
-    @IBOutlet weak var locationTextField: UITextField!
-    @IBOutlet weak var recommendationTextField: UITextField!
+    @IBOutlet var textFieldArray: [UITextField]!
     @IBOutlet weak var joinButton: UIButton!
     @IBOutlet weak var additionalInformationLabel: UILabel!
     @IBOutlet weak var additionalInformationSwitch: UISwitch!
-    
-    private lazy var textFieldArray = [idTextField,
-                                    passwordTextField,
-                                    nicknameTextField,
-                                    locationTextField,
-                                    recommendationTextField]
     
     private let placeholderArray = ["이메일 주소 또는 전화번호",
                                 "비밀번호",
@@ -40,31 +30,40 @@ final class JoinViewController: UIViewController {
     }
     
     // MARK: - methods
-    func configureUI() {
-        // View
+    private func configureUI() {
+        configureView()
+        configureLabel()
+        configureTextField()
+        configureButton()
+        configureSwitch()
+    }
+    
+    private func configureView() {
         view.backgroundColor = .black
+    }
+    
+    private func configureLabel() {
+        titleLabel.setLabel(text: "JEONGFLIX",
+                            color: UIColor(red: 236/255, green: 83/255, blue: 66/255, alpha: 1),
+                            font: .systemFont(ofSize: 28, weight: .black))
         
-        // Label
-        titleLabel.text = "JEONGFLIX"
-        titleLabel.textColor = UIColor(red: 236/255, green: 83/255, blue: 66/255, alpha: 1)
-        titleLabel.font = .systemFont(ofSize: 28, weight: .black)
-        
-        additionalInformationLabel.text = "추가 정보 입력"
-        additionalInformationLabel.textColor = .white
-        additionalInformationLabel.font = .systemFont(ofSize: 15, weight: .semibold)
-        
-        // TextField
+        additionalInformationLabel.setLabel(text: "추가 정보 입력",
+                                        color: .white,
+                                        font: .systemFont(ofSize: 15, weight: .semibold))
+    }
+    
+    private func configureTextField() {
         textFieldArray.enumerated().forEach { (i, e) in
-            e?.tintColor = .white
-            e?.textColor = .white
-            e?.textAlignment = .center
-            e?.backgroundColor = UIColor(red: 85/255, green: 85/255, blue: 85/255, alpha: 1)
-            e?.setPlaceholder(placeholder: placeholderArray[i], color: .white)
+            e.tintColor = .white
+            e.textColor = .white
+            e.textAlignment = .center
+            e.backgroundColor = UIColor(red: 85/255, green: 85/255, blue: 85/255, alpha: 1)
+            e.isSecureTextEntry = (i == 1) ? true : false
+            e.setPlaceholder(placeholder: placeholderArray[i], color: .white)
         }
-        
-        passwordTextField.isSecureTextEntry = true
-        
-        // Button
+    }
+    
+    private func configureButton() {
         let attributes: [NSAttributedString.Key: Any] = [.font: UIFont.systemFont(ofSize: 16, weight: .black),
                                                     .foregroundColor: UIColor.black]
         let attributedTitle = NSAttributedString(string: "회원가입", attributes: attributes)
@@ -73,8 +72,9 @@ final class JoinViewController: UIViewController {
         joinButton.layer.cornerRadius = 6
         joinButton.clipsToBounds = true
         joinButton.configuration?.contentInsets = .init(top: 14, leading: 0, bottom: 14, trailing: 0)
-        
-        // Switch
+    }
+    
+    private func configureSwitch() {
         additionalInformationSwitch.onTintColor = UIColor(red: 207/255, green: 79/255, blue: 64/255, alpha: 1)
         additionalInformationSwitch.backgroundColor = .lightGray
         additionalInformationSwitch.layer.cornerRadius = 16
@@ -88,7 +88,15 @@ final class JoinViewController: UIViewController {
 // MARK: - extensions
 extension UITextField {
     func setPlaceholder(placeholder: String, color: UIColor) {
-        attributedPlaceholder = NSAttributedString(string: placeholder,
-                                            attributes: [.foregroundColor: color])
+        self.attributedPlaceholder = NSAttributedString(string: placeholder,
+                                                attributes: [.foregroundColor: color])
+    }
+}
+
+extension UILabel {
+    func setLabel(text: String, color: UIColor, font: UIFont) {
+        self.text = text
+        self.textColor = color
+        self.font = font
     }
 }
