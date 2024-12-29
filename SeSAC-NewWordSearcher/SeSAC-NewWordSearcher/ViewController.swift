5af16e813c17a8bcd975a450752b0c6898ccd84a
KJH <gh7052@gmail.com>
2024-12-30 01:35:49 +0900

    add: SeSAC-NewWordSearcher

---
+++ SeSAC-NewWordSearcher/SeSAC-NewWordSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC-NewWordSearcher
+//
+//  Created by 김정호 on 12/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
