3adc9025a63cd421291e8a0bd13e9c236e6e74b2
KJH <gh7052@gmail.com>
2024-12-30 19:20:25 +0900

    add: SeSAC-BMICalculator

---
+++ SeSAC-BMICalculator/SeSAC-BMICalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC-BMICalculator
+//
+//  Created by 김정호 on 12/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
