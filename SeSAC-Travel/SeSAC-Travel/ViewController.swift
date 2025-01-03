847108eb2cc4ac378a76d52c5259c05f1f4c5a32
KJH <gh7052@gmail.com>
2025-01-03 19:48:03 +0900

    add: SeSAC-Travel

---
+++ SeSAC-Travel/SeSAC-Travel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC-Travel
+//
+//  Created by 김정호 on 1/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
