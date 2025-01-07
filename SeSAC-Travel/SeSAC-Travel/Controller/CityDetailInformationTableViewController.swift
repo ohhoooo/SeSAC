2dfff9a2b56f341e4a67caabba2bcaf821020765
KJH <gh7052@gmail.com>
2025-01-08 02:29:58 +0900

    feat(SeSAC-Travel): city detail information 화면 city detail information advertisement cell 추가에 따른 TableView 로직 추가 구현

--- SeSAC-Travel/SeSAC-Travel/Controller/CityDetailInformationTableViewController.swift
+++ SeSAC-Travel/SeSAC-Travel/Controller/CityDetailInformationTableViewController.swift
@@ -11,6 +11,7 @@ final class CityDetailInformationTableViewController: UITableViewController {
     
     // MARK: - properties
     private var travelArray = TravelInfo().travel
+    private let adArray = ["하와이 여행을 가고싶다면?\n수업이 있는데 가실 생각은 아니시죠?", "도쿄 여행 예약은?\nXCode로 직접 예약앱을 만들면 되겠네요"]
     
     // MARK: - life cycles
     override func viewDidLoad() {
@@ -42,7 +43,7 @@ final class CityDetailInformationTableViewController: UITableViewController {
 // MARK: - extensions
 extension CityDetailInformationTableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 140
+        return travelArray[indexPath.row].ad ? 80 : 140
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -50,14 +51,28 @@ extension CityDetailInformationTableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: CityDetailInformationTableViewCell.identifier, for: indexPath) as? CityDetailInformationTableViewCell else { return UITableViewCell() }
-        
-        cell.selectionStyle = .none
-        cell.configureData(travel: travelArray[indexPath.row])
-        cell.cityLikeButton.tag = indexPath.row
-        cell.cityLikeButton.addTarget(self, action: #selector(tappedCityLikeButton), for: .touchUpInside)
-        
-        return cell
+        if travelArray[indexPath.row].ad {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CityDetailInformationAdvertisementTableViewCell.identifier, for: indexPath) as? CityDetailInformationAdvertisementTableViewCell else { return UITableViewCell() }
+            
+            cell.selectionStyle = .none
+            cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
+            cell.configureData(title: adArray[indexPath.row % 2], isEven: (indexPath.row % 2 == 0))
+            
+            return cell
+        } else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CityDetailInformationTableViewCell.identifier, for: indexPath) as? CityDetailInformationTableViewCell else { return UITableViewCell() }
+            
+            if travelArray.count - 1 > indexPath.row && travelArray[indexPath.row + 1].ad {
+                cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
+            }
+            
+            cell.selectionStyle = .none
+            cell.configureData(travel: travelArray[indexPath.row])
+            cell.cityLikeButton.tag = indexPath.row
+            cell.cityLikeButton.addTarget(self, action: #selector(tappedCityLikeButton), for: .touchUpInside)
+            
+            return cell
+        }
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
