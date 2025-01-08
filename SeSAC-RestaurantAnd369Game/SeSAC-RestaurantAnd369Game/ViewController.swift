f3046bb22500fe285c3dd36db208653bfb271048
KJH <gh7052@gmail.com>
2025-01-08 17:57:27 +0900

    add: SeSAC-RestaurantAnd369Game

---
+++ SeSAC-RestaurantAnd369Game/SeSAC-RestaurantAnd369Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC-RestaurantAnd369Game
+//
+//  Created by 김정호 on 1/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
