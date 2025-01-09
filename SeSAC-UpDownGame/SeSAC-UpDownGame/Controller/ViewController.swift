891934418fe19dd6cf1e370a6672dd15ea7e3171
KJH <gh7052@gmail.com>
2025-01-09 16:04:00 +0900

    add: SeSAC-UpDownGame

---
+++ SeSAC-UpDownGame/SeSAC-UpDownGame/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC-UpDownGame
+//
+//  Created by 김정호 on 1/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
