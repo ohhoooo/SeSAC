fe3b63517246c515cfb3308943fccf91f31e616b
KJH <gh7052@gmail.com>
2025-01-10 19:44:53 +0900

    add: SeSAC-TravelTalk

---
+++ SeSAC-TravelTalk/SeSAC-TravelTalk/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC-TravelTalk
+//
+//  Created by 김정호 on 1/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
