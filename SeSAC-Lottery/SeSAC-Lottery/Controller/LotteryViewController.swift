d5aeb8a7013de73327ed42d50074c843e8deac1f
KJH <gh7052@gmail.com>
2025-01-14 15:41:04 +0900

    add: SeSAC-Lottery

---
+++ SeSAC-Lottery/SeSAC-Lottery/Controller/LotteryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LotteryViewController.swift
+//  SeSAC-Lottery
+//
+//  Created by 김정호 on 1/14/25.
+//
+
+import UIKit
+
+class LotteryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SeSAC-Lottery/SeSAC-Lottery/View/LotteryView.swift
@@ -0,0 +1,20 @@
+//
+//  LotteryView.swift
+//  SeSAC-Lottery
+//
+//  Created by 김정호 on 1/14/25.
+//
+
+import UIKit
+
+class LotteryView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
