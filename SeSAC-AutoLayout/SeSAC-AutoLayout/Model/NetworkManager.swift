1dd23605f01af6e9c4fd53361d33a485f774d4c6
KJH <gh7052@gmail.com>
2025-01-15 20:48:08 +0900

    feat(SeSAC-AutoLayout): NetworkManager에서 네트워크 통신을 통해 Movie 데이터를 받아와 Result 타입으로 반환하는 메서드 구현

--- SeSAC-AutoLayout/SeSAC-AutoLayout/Model/NetworkManager.swift
+++ SeSAC-AutoLayout/SeSAC-AutoLayout/Model/NetworkManager.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import Alamofire
+
+final class NetworkManager {
+    
+    // MARK: - properties
+    static let shared = NetworkManager()
+    private init() {}
+    
+    // base
+    private let baseUrl = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
+    
+    // key
+    private let apiKey = Bundle.main.infoDictionary?["APIKey"] as! String
+    
+    // MARK: - methods
+    func fetchMovieList(date: String, completion: @escaping ((Result<Movie, Error>) -> Void)) {
+        let parameters: Parameters = ["targetDt": date]
+        
+        AF.request(baseUrl + "?key=\(apiKey)", parameters: parameters).responseDecodable(of: Movie.self) { response in
+            switch response.result {
+            case .success(let Movie):
+                completion(.success(Movie))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
