9d330f6e0d83c4d010a17c97b3863bce8f201e4a
KJH <gh7052@gmail.com>
2025-01-16 01:30:17 +0900

    feat(SeSAC-Shopping): Shopping 데이터 모델 구성

---
+++ SeSAC-Shopping/SeSAC-Shopping/Model/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  SeSAC-Shopping
+//
+//  Created by 김정호 on 1/16/25.
+//
+
+import Foundation
---
+++ SeSAC-Shopping/SeSAC-Shopping/Model/Shopping.swift
@@ -0,0 +1,27 @@
+//
+//  Shopping.swift
+//  SeSAC-Shopping
+//
+//  Created by 김정호 on 1/16/25.
+//
+
+import Foundation
+
+struct Shopping: Decodable {
+    let total: Int
+    let items: [Item]
+}
+
+struct Item: Decodable {
+    let title: String?
+    let image: String?
+    let price: String?
+    let mallName: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case image
+        case price = "lprice"
+        case mallName
+    }
+}
