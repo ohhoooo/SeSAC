48978bbe7853457d652d12153cb7d429d163e4ea
KJH <gh7052@gmail.com>
2025-01-16 01:45:01 +0900

    feat(SeSAC-Shopping): NetworkManager에서 네트워크 통신을 통해 Shopping 데이터를 받아와 Result 타입으로 반환하는 메서드 구현

--- SeSAC-Shopping/SeSAC-Shopping/Model/NetworkManager.swift
+++ SeSAC-Shopping/SeSAC-Shopping/Model/NetworkManager.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import Alamofire
+
+final class NetworkManager {
+    
+    // MARK: - properties
+    static let shared = NetworkManager()
+    private init() {}
+    
+    // header key
+    private let clientId = Bundle.main.infoDictionary?["ClientId"] as! String
+    private let clientSecret = Bundle.main.infoDictionary?["ClientSecret"] as! String
+    
+    // header
+    private lazy var headers: HTTPHeaders = ["X-Naver-Client-Id": clientId, "X-Naver-Client-Secret": clientSecret]
+    
+    // base
+    private let baseUrl = "https://openapi.naver.com/v1/search/shop.json"
+    
+    // MARK: - methods
+    func fetchShopping(query: String, completion: @escaping ((Result<Shopping, Error>) -> Void)) {
+        let parameters: Parameters = ["query": query, "display": "100"]
+        
+        AF.request(baseUrl, parameters: parameters, headers: headers).responseDecodable(of: Shopping.self) { response in
+            switch response.result {
+            case .success(let shopping):
+                completion(.success(shopping))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
