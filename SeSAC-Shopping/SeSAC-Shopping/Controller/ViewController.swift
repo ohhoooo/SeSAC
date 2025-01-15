f904042b7a0dc698ccd71d976bed989e33f1c75c
KJH <gh7052@gmail.com>
2025-01-15 21:56:25 +0900

    add: SeSAC-Shopping

---
+++ SeSAC-Shopping/SeSAC-Shopping/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC-Shopping
+//
+//  Created by 김정호 on 1/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
