1a4f794ba972fc158d674568e1a3a8ad30afd2fe
KJH <gh7052@gmail.com>
2025-01-16 20:05:31 +0900

    feat(SeSAC-Shopping): BaseViewProtocol, BaseView 구현

---
+++ SeSAC-Shopping/SeSAC-Shopping/Util/Base/BaseView.swift
@@ -0,0 +1,38 @@
+//
+//  BaseView.swift
+//  SeSAC-Shopping
+//
+//  Created by 김정호 on 1/16/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+protocol BaseViewProtocol {
+    func configureUI()
+    func configureHierarchy()
+    func configureConstraints()
+}
+
+class BaseView: UIView, BaseViewProtocol {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+        configureHierarchy()
+        configureConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI() { }
+    
+    func configureHierarchy() { }
+    
+    func configureConstraints() { }
+}
