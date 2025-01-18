55192989546315c2723d23015df0110bd3f5a866
KJH <gh7052@gmail.com>
2025-01-18 14:49:20 +0900

    feat(SeSAC-Photo): BaseView, BaseCollectionViewCell, BaseViewController 구현

---
+++ SeSAC-Photo/SeSAC-Photo/Util/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  BaseCollectionViewCell.swift
+//  SeSAC-Photo
+//
+//  Created by 김정호 on 1/18/25.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell, BaseViewProtocol {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+        configureHierarchy()
+        configureConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI() { }
+    
+    func configureHierarchy() { }
+    
+    func configureConstraints() { }
+}
