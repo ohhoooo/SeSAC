9df11666ed1cfd9df983e8583219470449b0a7f0
KJH <gh7052@gmail.com>
2025-01-18 20:37:15 +0900

    feat(SeSAC-Photo): NetworkManager 검색 결과에 해당하는 Photo 리스트를 가져오는 메소드 추가

--- SeSAC-Photo/SeSAC-Photo/Model/NetworkManager.swift
+++ SeSAC-Photo/SeSAC-Photo/Model/NetworkManager.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import Alamofire
+
+final class NetworkManager {
+    
+    // MARK: - properties
+    static let shared = NetworkManager()
+    private init() {}
+    
+    // header key
+    private let apiKey = Bundle.main.infoDictionary?["APIKey"] as! String
+    
+    // header
+    private lazy var headers: HTTPHeaders = ["Authorization": apiKey]
+    
+    // base
+    private let baseUrl = "https://api.unsplash.com"
+    
+    // path
+    private let searchPath = "/search/photos"
+    
+    // MARK: - methods
+    func fetchPhotoSearch(query: String,
+                          page: Int,
+                          perPage: Int,
+                          orderBy: String,
+                          completion: @escaping ((Result<PhotoSearch, Error>) -> Void)) {
+        let parameters: Parameters = ["query": query, "page": page, "per_page": perPage, "order_by": orderBy]
+        
+        AF.request(baseUrl + searchPath, parameters: parameters, headers: headers).responseDecodable(of: PhotoSearch.self) { response in
+            switch response.result {
+            case .success(let photoSearch):
+                completion(.success(photoSearch))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
