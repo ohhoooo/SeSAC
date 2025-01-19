d9dd130a5e830d917119f25d62e705de7667eec1
KJH <gh7052@gmail.com>
2025-01-20 00:46:06 +0900

    feat(SeSAC-Photo): photo search cell 화면 Code Base로 UI 구현

--- SeSAC-Photo/SeSAC-Photo/View/PhotoSearchCollectionViewCell.swift
+++ SeSAC-Photo/SeSAC-Photo/View/PhotoSearchCollectionViewCell.swift
@@ -6,9 +6,67 @@
 //
 
 import UIKit
+import Kingfisher
+import SnapKit
+import Then
 
-class PhotoSearchCollectionViewCell: UICollectionViewCell {
+final class PhotoSearchCollectionViewCell: BaseCollectionViewCell {
     
+    // MARK: - properties
+    static let identifier = "PhotoSearchCollectionViewCell"
+    
+    private let photoImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+    }
+    
+    private let likeButton = UIButton()
+    
+    // MARK: - life cycles
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        photoImageView.image = nil
+        likeButton.configuration = nil
+    }
+    
+    // MARK: - methods
+    override func configureUI() {
+        configureLikeButton()
+    }
+    
+    override func configureHierarchy() {
+        contentView.addSubview(photoImageView)
+        contentView.addSubview(likeButton)
+    }
+    
+    override func configureConstraints() {
+        photoImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        likeButton.snp.makeConstraints {
+            $0.leading.bottom.equalToSuperview().inset(16)
+        }
+    }
+    
+    func configureLikeButton(like: Int? = 0) {
+        var config = UIButton.Configuration.filled()
+        config.title = String((like ?? 0).formatted(.number))
+        config.image = UIImage(systemName: "star.fill")?.resize(width: 15, height: 13).withTintColor(UIColor(red: 236/255, green: 216/255, blue: 125/255, alpha: 1))
+        config.imagePadding = 4
+        config.baseForegroundColor = .white
+        config.baseBackgroundColor = UIColor(red: 81/255, green: 89/255, blue: 86/255, alpha: 1)
+        config.attributedTitle?.font = UIFont.systemFont(ofSize: 11)
+        likeButton.configuration = config
+        likeButton.clipsToBounds = true
+        likeButton.layer.cornerRadius = 14
+    }
+    
+    func bind(photo: Photo?) {
+        configureLikeButton(like: photo?.likes)
+        
+        guard let url = photo?.urls?.small else { return }
+        photoImageView.kf.setImage(with: URL(string: url))
+    }
 }
-
-// MARK: - extensions
