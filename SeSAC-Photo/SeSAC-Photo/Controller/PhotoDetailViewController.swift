c7906b0061409d2385438927ac04eff7dd074520
KJH <gh7052@gmail.com>
2025-01-20 20:44:46 +0900

    feat(SeSAC-Photo): photo detail 화면 이전 화면에서 넘어온 데이터와 네트워크 통신을 통해 반환된 결과 데이터를 함께 뷰에 전달하는 기능 추가

--- SeSAC-Photo/SeSAC-Photo/Controller/PhotoDetailViewController.swift
+++ SeSAC-Photo/SeSAC-Photo/Controller/PhotoDetailViewController.swift
@@ -7,23 +7,44 @@
 
 import UIKit
 
-class PhotoDetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class PhotoDetailViewController: BaseViewController {
+    
+    // MARK: - properties
+    private let photoDetailView = PhotoDetailView()
+    
+    var photo: Photo? {
+        didSet {
+            bind()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - life cycles
+    override func loadView() {
+        view = photoDetailView
     }
-    */
-
+    
+    // MARK: - methods
+    override func configureStyle() {
+        configureNavigation()
+    }
+    
+    override func bind() {
+        guard let photo else { return }
+        guard let photoId = photo.id else { return photoDetailView.bind(photo: photo) }
+        
+        NetworkManager.shared.fetchPhotoStatistic(photoId: photoId) { result in
+            switch result {
+            case .success(let photoStatistic):
+                self.photoDetailView.bind(photo: photo, photoStatistic: photoStatistic)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    private func configureNavigation() {
+        navigationItem.title = ""
+        navigationController?.navigationBar.tintColor = .black
+        navigationController?.navigationBar.topItem?.title = ""
+    }
 }
