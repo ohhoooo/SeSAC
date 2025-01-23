00ff208d89007052c35b0432386155647b2ea84c
KJH <gh7052@gmail.com>
2025-01-23 22:28:21 +0900

    feat(SeSAC-Photo): profile 화면 Code Base로 UI 구현

--- SeSAC-Photo/SeSAC-Photo/Controller/Profile/ProfileViewController.swift
+++ SeSAC-Photo/SeSAC-Photo/Controller/Profile/ProfileViewController.swift
@@ -7,23 +7,43 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class ProfileViewController: BaseViewController {
+    
+    // MARK: - properties
+    private let profileView = ProfileView()
+    
+    // MARK: - life cycles
+    override func loadView() {
+        view = profileView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - methods
+    override func configureStyle() {
+        configureNavigation()
     }
-    */
-
+    
+    override func configureAddTarget() {
+        profileView.nicknameButton.addTarget(self, action: #selector(tappedChangeButton), for: .touchUpInside)
+        profileView.birthdayButton.addTarget(self, action: #selector(tappedChangeButton), for: .touchUpInside)
+        profileView.levelButton.addTarget(self, action: #selector(tappedChangeButton), for: .touchUpInside)
+    }
+    
+    private func configureNavigation() {
+        navigationItem.title = "프로필 화면"
+        navigationItem.rightBarButtonItem = profileView.withdrawBarButtonItem
+        navigationController?.navigationBar.tintColor = .black
+        
+        profileView.withdrawBarButtonItem.target = self
+        profileView.withdrawBarButtonItem.action = #selector(tappedWithdrawBarButtonItem)
+    }
+    
+    @objc
+    private func tappedChangeButton(_ sender: UIButton) {
+        print(#function)
+    }
+    
+    @objc
+    private func tappedWithdrawBarButtonItem() {
+        print(#function)
+    }
 }
--- SeSAC-Photo/SeSAC-Photo/View/Profile/ProfileView.swift
+++ SeSAC-Photo/SeSAC-Photo/View/Profile/ProfileView.swift
@@ -6,15 +6,104 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class ProfileView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class ProfileView: BaseView {
+    
+    // MARK: - properties
+    let withdrawBarButtonItem = UIBarButtonItem().then {
+        $0.style = .plain
+        $0.title = "탈퇴하기"
     }
-    */
-
+    
+    let nicknameButton = UIButton().then {
+        $0.setTitleColor(.black, for: .normal)
+        $0.setTitle("닉네임", for: .normal)
+    }
+    
+    private let nicknameLabel = UILabel().then {
+        $0.text = "NO NAME"
+        $0.textColor = .lightGray
+        $0.textAlignment = .right
+    }
+    
+    let birthdayButton = UIButton().then {
+        $0.setTitleColor(.black, for: .normal)
+        $0.setTitle("생일", for: .normal)
+    }
+    
+    private let birthdayLabel = UILabel().then {
+        $0.text = "NO DATE"
+        $0.textColor = .lightGray
+        $0.textAlignment = .right
+    }
+    
+    let levelButton = UIButton().then {
+        $0.setTitleColor(.black, for: .normal)
+        $0.setTitle("레벨", for: .normal)
+    }
+    
+    private let levelLabel = UILabel().then {
+        $0.text = "NO LEVEL"
+        $0.textColor = .lightGray
+        $0.textAlignment = .right
+    }
+    
+    // MARK: - methods
+    override func configureUI() {
+        backgroundColor = .white
+    }
+    
+    override func configureHierarchy() {
+        addSubview(nicknameButton)
+        addSubview(nicknameLabel)
+        addSubview(birthdayButton)
+        addSubview(birthdayLabel)
+        addSubview(levelButton)
+        addSubview(levelLabel)
+    }
+    
+    override func configureConstraints() {
+        nicknameButton.snp.makeConstraints {
+            $0.leading.top.equalTo(safeAreaLayoutGuide).inset(24)
+            $0.height.equalTo(50)
+            $0.width.equalTo(100)
+        }
+        
+        nicknameLabel.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide).offset(24)
+            $0.trailing.equalTo(safeAreaLayoutGuide).inset(24)
+            $0.leading.equalTo(nicknameButton.snp.trailing).offset(24)
+            $0.height.equalTo(50)
+        }
+        
+        birthdayButton.snp.makeConstraints {
+            $0.leading.equalTo(safeAreaLayoutGuide).offset(24)
+            $0.top.equalTo(nicknameButton.snp.bottom).offset(24)
+            $0.height.equalTo(50)
+            $0.width.equalTo(100)
+        }
+        
+        birthdayLabel.snp.makeConstraints {
+            $0.top.equalTo(nicknameLabel.snp.bottom).offset(24)
+            $0.trailing.equalTo(safeAreaLayoutGuide).inset(24)
+            $0.leading.equalTo(birthdayButton.snp.trailing).offset(24)
+            $0.height.equalTo(50)
+        }
+        
+        levelButton.snp.makeConstraints {
+            $0.leading.equalTo(safeAreaLayoutGuide).offset(24)
+            $0.top.equalTo(birthdayButton.snp.bottom).offset(24)
+            $0.height.equalTo(50)
+            $0.width.equalTo(100)
+        }
+        
+        levelLabel.snp.makeConstraints {
+            $0.top.equalTo(birthdayLabel.snp.bottom).offset(24)
+            $0.trailing.equalTo(safeAreaLayoutGuide).inset(24)
+            $0.leading.equalTo(levelButton.snp.trailing).offset(24)
+            $0.height.equalTo(50)
+        }
+    }
 }
