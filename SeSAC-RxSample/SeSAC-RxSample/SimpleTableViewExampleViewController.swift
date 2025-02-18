004a7abdba3c8179827c49297cebb01720d7e3ee
KJH <gh7052@gmail.com>
2025-02-18 22:58:35 +0900

    add: SeSAC-RxSample

---
+++ SeSAC-RxSample/SeSAC-RxSample/SimpleTableViewExampleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTableViewExampleViewController.swift
+//  SeSAC-RxSample
+//
+//  Created by 김정호 on 2/18/25.
+//
+
+import UIKit
+
+final class SimpleTableViewExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
