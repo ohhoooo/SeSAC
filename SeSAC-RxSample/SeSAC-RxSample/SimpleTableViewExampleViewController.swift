5b3e4d2179c80cd0ad4a45830c2dd71bb30f4a48
KJH <gh7052@gmail.com>
2025-02-19 01:56:07 +0900

    feat(SeSAC-RxSample): SimpleTableViewExampleViewController 테이블뷰 셀 클릭, 악세사리 클릭 시 얼럿 띄우는 기능을 rx로 구현

--- SeSAC-RxSample/SeSAC-RxSample/SimpleTableViewExampleViewController.swift
+++ SeSAC-RxSample/SeSAC-RxSample/SimpleTableViewExampleViewController.swift
@@ -6,12 +6,73 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
 
 final class SimpleTableViewExampleViewController: UIViewController {
-
+    
+    // MARK: - properties
+    private let tableView = UITableView()
+    
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        configureTableView()
+    }
+    
+    // MARK: - methods
+    private func configureTableView() {
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        let items = Observable.just(
+            (0..<20).map { "\($0)" }
+        )
+        
+        items
+            .bind(to: tableView.rx.items(cellIdentifier: "Cell", cellType: UITableViewCell.self)) { (row, element, cell) in
+                cell.textLabel?.text = "\(element) @ row \(row)"
+                cell.accessoryType = .detailButton
+            }
+            .disposed(by: disposeBag)
+        
+        tableView
+            .rx
+            .modelSelected(String.self)
+            .subscribe(with: self) { owner, value in
+                owner.showAlert(title: "modelSelected", message: "Tapped \(value)")
+            }
+            .disposed(by: disposeBag)
+        
+        tableView
+            .rx
+            .itemAccessoryButtonTapped
+            .subscribe(with: self) { owner, value in
+                owner.showAlert(title: "itemAccessoryButtonTapped", message: "Tapped \(value)")
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alertVC = UIAlertController(title: title,
+                                        message: message,
+                                        preferredStyle: .alert)
+        
+        let ok = UIAlertAction(title: "확인", style: .default)
+        
+        alertVC.addAction(ok)
+        present(alertVC, animated: true)
     }
 }
