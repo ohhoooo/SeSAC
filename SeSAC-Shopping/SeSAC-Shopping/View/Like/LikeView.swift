a66dba52d4a6ab3da0b1b0ea0f163509c17990f1
KJH <gh7052@gmail.com>
2025-03-05 02:24:40 +0900

    feat(SeSAC-Shopping): LikeView UI 구현

--- SeSAC-Shopping/SeSAC-Shopping/View/Like/LikeView.swift
+++ SeSAC-Shopping/SeSAC-Shopping/View/Like/LikeView.swift
@@ -6,15 +6,54 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class LikeView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class LikeView: BaseView {
+    
+    // MARK: - properties
+    let searchBar = UISearchBar().then {
+        $0.placeholder = "검색어를 입력하세요"
+        $0.searchBarStyle = .minimal
     }
-    */
-
+    
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout()).then {
+        $0.register(ResultCollectionViewCell.self, forCellWithReuseIdentifier: ResultCollectionViewCell.identifier)
+    }
+    
+    // MARK: - methods
+    override func configureUI() {
+        backgroundColor = .white
+    }
+    
+    override func configureHierarchy() {
+        addSubview(searchBar)
+        addSubview(collectionView)
+    }
+    
+    override func configureConstraints() {
+        searchBar.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide)
+            $0.horizontalEdges.equalToSuperview()
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(searchBar.snp.bottom)
+            $0.horizontalEdges.bottom.equalToSuperview()
+        }
+    }
+    
+    private func createLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        let lineSpacing: CGFloat = 0
+        let horizontalInset: CGFloat = 12
+        let interitemSpacing: CGFloat = 12
+        let width = (UIScreen.main.bounds.size.width - (horizontalInset * 2) - interitemSpacing) / 2
+        layout.itemSize = CGSize(width: width, height: width * 1.6)
+        layout.sectionInset = UIEdgeInsets(top: 0, left: horizontalInset, bottom: 0, right: horizontalInset)
+        layout.minimumLineSpacing = lineSpacing
+        layout.minimumInteritemSpacing = interitemSpacing
+        
+        return layout
+    }
 }
