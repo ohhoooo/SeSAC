1008fd41c76d400f77b455ae9579f5f0486339ee
KJH <gh7052@gmail.com>
2025-03-06 04:23:03 +0900

    feat(SeSAC-Shopping): FolderViewController 데이터 바인딩 및 Cell 클릭 시 화면 이동 기능 구현

--- SeSAC-Shopping/SeSAC-Shopping/View/Folder/FolderViewController.swift
+++ SeSAC-Shopping/SeSAC-Shopping/View/Folder/FolderViewController.swift
@@ -6,24 +6,59 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class FolderViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class FolderViewController: BaseViewController {
+    
+    // MARK: - properties
+    private let folderView = FolderView()
+    private let viewModel = FolderViewModel()
+    
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - life cycles
+    override func loadView() {
+        view = folderView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        configureNavigation()
+        folderView.tableView.reloadData()
     }
-    */
-
+    
+    // MARK: - methods
+    override func bind() {
+        let input = FolderViewModel.Input(
+            modelSelected: folderView.tableView.rx.modelSelected(Folder.self)
+        )
+        
+        let output = viewModel.transform(input: input)
+        
+        output.items
+            .bind(to: folderView.tableView.rx.items(
+                cellIdentifier: FolderCell.identifier,
+                cellType: FolderCell.self)
+            ) { (row, element, cell) in
+                cell.bind(folder: element)
+            }
+            .disposed(by: disposeBag)
+        
+        output.modelSelected
+            .bind(with: self) { owner, value in
+                let wishListVM = WishListViewModel(id: value.id)
+                let wishListVC = WishListViewController(viewModel: wishListVM)
+                
+                owner.navigationController?.pushViewController(wishListVC, animated: true)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func configureNavigation() {
+        navigationItem.title = "폴더 목록"
+        navigationController?.navigationBar.topItem?.title = ""
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.black]
+    }
 }
