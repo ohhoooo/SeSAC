3635e00eca00a4270c7060925c41e89fced891a5
KJH <gh7052@gmail.com>
2025-03-06 04:35:23 +0900

    feat(SeSAC-Shopping): WishListTableRepository, WishListViewModel 구현

--- SeSAC-Shopping/SeSAC-Shopping/Model/Repository/WishListTableRepository.swift
+++ SeSAC-Shopping/SeSAC-Shopping/Model/Repository/WishListTableRepository.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import RealmSwift
+
+protocol WishListRepository {
+    func fetchAllItems(id: ObjectId) -> [WishList]
+    func createItem(folder: Folder, title: String) -> [WishList]
+    func deleteItem(id: ObjectId, wishList: WishList) -> [WishList]
+}
+
+final class WishListTableRepository: WishListRepository {
+    
+    // MARK: - properties
+    private let realm = try! Realm()
+    
+    // MARK: - methods
+    func fetchAllItems(id: ObjectId) -> [WishList] {
+        return Array(realm.objects(Folder.self).where { $0.id == id }.first!.wishList)
+    }
+    
+    func createItem(folder: Folder, title: String) -> [WishList] {
+        do {
+            try realm.write {
+                let wishList = WishList(title: title)
+                folder.wishList.append(wishList)
+            }
+        } catch {
+            print("WishList 저장 실패")
+        }
+        
+        return fetchAllItems(id: folder.id)
+    }
+    
+    func deleteItem(id: ObjectId, wishList: WishList) -> [WishList] {
+        do {
+            try realm.write {
+                realm.delete(wishList)
+            }
+        } catch {
+            print("WishList 삭제 실패")
+        }
+        
+        return fetchAllItems(id: id)
+    }
+}
--- SeSAC-Shopping/SeSAC-Shopping/ViewModel/WishListViewModel.swift
+++ SeSAC-Shopping/SeSAC-Shopping/ViewModel/WishListViewModel.swift
@@ -6,3 +6,66 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import RealmSwift
+
+final class WishListViewModel {
+    
+    // MARK: - properties
+    private let folderRepository: FolderRepository = FolderTableRepository()
+    private let wishListRepository: WishListRepository = WishListTableRepository()
+    
+    struct Input {
+        let changedSearchBarText: ControlProperty<String>
+        let searchButtonClicked: ControlEvent<Void>
+        let modelSelected: ControlEvent<IndexPath>
+    }
+    
+    struct Output {
+        let title: Observable<String>
+        let items: BehaviorRelay<[WishList]>
+    }
+    
+    private let items = BehaviorRelay<[WishList]>(value: [])
+    private var searchBarText = ""
+    private let id: ObjectId
+    
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - life cycles
+    init(id: ObjectId) {
+        self.id = id
+    }
+    
+    // MARK: - methods
+    func transform(input: Input) -> Output {
+        items.accept(wishListRepository.fetchAllItems(id: id))
+        
+        input.changedSearchBarText
+            .bind(with: self) { owner, value in
+                owner.searchBarText = value
+            }
+            .disposed(by: disposeBag)
+        
+        input.searchButtonClicked
+            .bind(with: self) { owner, _ in
+                let folder = owner.folderRepository.fetchItem(id: owner.id)
+                let wishList = owner.wishListRepository.createItem(folder: folder, title: owner.searchBarText)
+                owner.items.accept(wishList)
+            }
+            .disposed(by: disposeBag)
+        
+        input.modelSelected
+            .bind(with: self) { owner, value in
+                let wishList = owner.wishListRepository.deleteItem(id: owner.id, wishList: owner.items.value[value.item])
+                owner.items.accept(wishList)
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(
+            title: Observable.just(folderRepository.fetchItem(id: id).name),
+            items: items
+        )
+    }
+}
