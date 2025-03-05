acc4a9d210d72fe2040fb485872aba73c883a66d
KJH <gh7052@gmail.com>
2025-03-06 04:16:19 +0900

    feat(SeSAC-Shopping): FolderRepository, FolderViewModel 구현

--- SeSAC-Shopping/SeSAC-Shopping/Model/Repository/FolderTableRepository.swift
+++ SeSAC-Shopping/SeSAC-Shopping/Model/Repository/FolderTableRepository.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import RealmSwift
+
+protocol FolderRepository {
+    func fetchItem(id: ObjectId) -> Folder
+    func fetchAllItems() -> [Folder]
+    func createItems()
+}
+
+final class FolderTableRepository: FolderRepository {
+    
+    // MARK: - properties
+    private let realm = try! Realm()
+    
+    // MARK: - methods
+    func fetchItem(id: ObjectId) -> Folder {
+        return realm.objects(Folder.self).where { $0.id == id }.first!
+    }
+    
+    func fetchAllItems() -> [Folder] {
+        if realm.objects(Folder.self).isEmpty {
+            createItems()
+        }
+        
+        return Array(realm.objects(Folder.self))
+    }
+    
+    func createItems() {
+        do {
+            try realm.write {
+                let shopping = Folder(name: "쇼핑")
+                let travel = Folder(name: "여행")
+                let experience = Folder(name: "경험")
+                let study = Folder(name: "공부")
+                
+                realm.add(shopping)
+                realm.add(travel)
+                realm.add(experience)
+                realm.add(study)
+            }
+        } catch {
+            print("Folder 저장 실패")
+        }
+    }
+}
--- SeSAC-Shopping/SeSAC-Shopping/ViewModel/FolderViewModel.swift
+++ SeSAC-Shopping/SeSAC-Shopping/ViewModel/FolderViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+final class FolderViewModel {
+    
+    // MARK: - properties
+    private let repository: FolderRepository = FolderTableRepository()
+    
+    struct Input {
+        let modelSelected: ControlEvent<Folder>
+    }
+    
+    struct Output {
+        let items: Observable<[Folder]>
+        let modelSelected: ControlEvent<Folder>
+    }
+    
+    // MARK: - methods
+    func transform(input: Input) -> Output {
+        let items = Observable.just(repository.fetchAllItems())
+        
+        return Output(
+            items: items,
+            modelSelected: input.modelSelected
+        )
+    }
+}
